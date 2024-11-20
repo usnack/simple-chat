8a9d33457b909134af90de11370c986079f0b0fa
usnack <usnack522@gmail.com>
2024-11-20 10:09:49 +0900

    Initial Commit

---
+++ src/test/java/io/usnack/simplechat/SimpleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.usnack.simplechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
